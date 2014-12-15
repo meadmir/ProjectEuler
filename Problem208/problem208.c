42862399536ca8a526ff94388e703cbe79ab15c4
meadmir <admir.git@gmail.com>
2014-12-15 17:06:16 +0100

    Implemented moving function for robot.

--- Problem208/problem208.c
+++ Problem208/problem208.c
@@ -1,12 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include "problem208.h"
 
-char* problem208(void) {
-	float x = 0, y = 0;
-	int theta = 90;
-	const int alpha = 72;
+#define PI 3.14159265
+
+void move(State *s, Direction *d, const float alpha)
+{
+	float d_theta = d->h * d->v * alpha;
 	
-	for (int i = 0; i < 5; ++i) 
+	s->x += d->h * d->v * cos(s->theta + d_theta / 2);
+	s->y += d->h * d->v * sin(s->theta + d_theta / 2);
+	s->theta += d_theta;
+}
+
+char* problem208(void)
+{	
+	State state = {.x = 0, .y = 0, .theta = PI / 2};
 	
+	const float alpha = (2 * PI) / 5;
+	unsigned __int128 asd = 2;
+	move(&state, &((Direction) {1, 1}), alpha);
+	move(&state, &((Direction) {-1, -1}), alpha);
+	move(&state, &((Direction) {1, 1}), alpha);
+
 	return "str";
 }
--- Problem208/problem208.h
+++ Problem208/problem208.h
@@ -1,6 +1,20 @@
 #ifndef PROBLEM208
 #define PROBLEM208
 
+typedef struct direction {
+   short v;
+   short h;
+} Direction;
+
+typedef struct state {
+   float x; 
+   float y; 
+   float theta;
+} State;
+
 char* problem208(void);
+// h = 1 means going left, h = -1 means going right
+// v = 1 means going forward, v = -1 means going backwards
+void move(State *state, Direction *d, const float alpha);
 
 #endif
